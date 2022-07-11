32215081ac0eff1b864c7991b600bae52fb4584f
2Kelvin <kvin.mwangi254@gmail.com>
2022-07-11 10:10:01 +0300

    added whole layout Composables

--- app/src/main/java/com/example/jetpackcomposearticles/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposearticles/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.jetpackcomposearticles
 
 import android.os.Bundle
+import android.provider.Settings.System.getString
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposearticles.ui.theme.JetpackComposeArticlesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    WholePage(
+                        heading = stringResource(R.string.article_heading),
+                        paragraphText1 = stringResource(id = R.string.paragraph_1_text),
+                        paragraphText2 = stringResource(id = R.string.paragraph_2_text)
+                    )
                 }
             }
         }
@@ -30,14 +44,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ArticleImage() {
+    val pic = painterResource(R.drawable.bg_compose_background)
+    Image(
+        painter = pic,
+        contentDescription = "article image",
+//        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun TutorialHeading(heading:String) {
+    Text(
+        text = heading,
+        fontSize = 24.sp,
+        modifier = Modifier.padding(all = 16.dp)
+    )
+}
+
+@Composable
+fun paragraphOne(paragraphText1:String) {
+    Text(
+        text = paragraphText1,
+        Modifier.padding(start = 16.dp, end = 16.dp),
+        //justify.textalign
+    )
+}
+
+@Composable
+fun paragraphTwo(paragraphText2: String) {
+    Text(
+        text = paragraphText2,
+        Modifier.padding(all = 16.dp)
+        //justify.textalign
+    )
+}
+
+@Composable
+fun WholePage(heading: String, paragraphText1: String, paragraphText2: String) {
+    Column {
+        ArticleImage()
+        TutorialHeading(heading)
+        paragraphOne(paragraphText1)
+        paragraphTwo(paragraphText2)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeArticlesTheme {
-        Greeting("Android")
+        WholePage(
+            heading = stringResource(R.string.article_heading),
+            paragraphText1 = stringResource(id = R.string.paragraph_1_text),
+            paragraphText2 = stringResource(id = R.string.paragraph_2_text)
+        )
     }
 }
